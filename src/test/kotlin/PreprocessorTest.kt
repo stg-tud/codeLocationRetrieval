a7a76c02f1dc1448cf963c979fc010a89957a511
kebiro <bekiroe@hotmail.de>
2019-12-04 19:17:47 +0100

    Add simple tests for token and document extraction

--- src/test/kotlin/PreprocessorTest.kt
+++ src/test/kotlin/PreprocessorTest.kt
@@ -1,8 +1,9 @@
 
 import org.assertj.core.api.Assertions
-//import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import preprocessor.Preprocessor
+import preprocessor.TokenType
+import java.io.File
 
 class PreprocessorTest {
     private val preprocessor = Preprocessor()
@@ -41,5 +42,46 @@ class PreprocessorTest {
             // more than one underscore
         Assertions.assertThat(preprocessor.getModifiedIdentifier("_under_score_"))
             .isEqualTo("under score")
+    }
+
+    @Test
+    fun testTokenExtraction() {
+        val expectedOutput = File("src/test/resources/testExpectedOutput/main/corpus.txt")
+            .readText()
+            .replace("\r", "")
+
+        val actualOutput = StringBuilder()
+        val tokenList = preprocessor.extractTokens(File("src/test/resources/testInput/main.c").readText())
+        for(token in tokenList) {
+            when(token.tokenType) {
+                TokenType.COMMENT -> { actualOutput.append(token.value) }
+                TokenType.IDENTIFIER -> {
+                    actualOutput.append(token.value)
+                    val modified = preprocessor.getModifiedIdentifier(token.value)
+                    if(modified != null) {
+                        actualOutput.append("\n")
+                        actualOutput.append(modified)
+                    }
+                }
+            }
+
+            actualOutput.append("\n")
+        }
+
+        Assertions.assertThat(actualOutput.toString()).isEqualTo(expectedOutput)
+    }
+
+    @Test
+    fun testDocumentExtraction() {
+        val expectedOutput = File("src/test/resources/testExpectedOutput/main/doc0.txt")
+            .readText()
+            .replace("\r", "")
+
+        val actualOutput = preprocessor.extractDocuments(File("src/test/resources/testInput/main.c").readText())
+
+        Assertions.assertThat(actualOutput.size).isEqualTo(1)
+        // TODO: whitespace is causing test to fail
+//        Assertions.assertThat(actualOutput[0]).isEqualTo(expectedOutput)
+        Assertions.assertThat(actualOutput[0]).isEqualToIgnoringWhitespace(expectedOutput)
     }
 }
