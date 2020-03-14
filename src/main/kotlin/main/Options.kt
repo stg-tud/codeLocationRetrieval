67d576260240da38afbe44a7986859a84a7fd37d
kebiro <bekiroe@hotmail.de>
2020-03-14 12:36:56 +0100

    Hardcode the default stop-list into a member field

--- src/main/kotlin/main/Options.kt
+++ src/main/kotlin/main/Options.kt
@@ -51,6 +51,84 @@ object Options {
     lateinit var outputSvdFile: File
         private set
 
+    // ==========
+    // == Misc ==
+    // ==========
+
+    private val defaultStopList = listOf(
+        "a", "able", "about", "above", "abst", "accordance", "according", "accordingly", "across", "act", "actually",
+        "added", "adj", "affected", "affecting", "affects", "after", "afterwards", "again", "against", "ah", "all",
+        "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "an", "and",
+        "announce", "another", "any", "anybody", "anyhow", "anymore", "anyone", "anything", "anyway", "anyways",
+        "anywhere", "apparently", "approximately", "are", "aren", "arent", "arise", "around", "as", "aside", "ask",
+        "asking", "at", "auth", "available", "away", "awfully",
+        "b", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "begin",
+        "beginning", "beginnings", "begins", "behind", "being", "believe", "below", "beside", "besides", "between",
+        "beyond", "biol", "both", "brief", "briefly", "but", "by",
+        "c", "ca", "came", "can", "cannot", "can't",
+        "cause", "causes", "certain", "certainly", "co", "com", "come", "comes", "contain", "containing", "contains",
+        "could", "couldnt",
+        "d", "date", "did", "didn't", "different", "do", "does", "doesn't", "doing", "done", "don't",
+        "down", "downwards", "due", "during", "e", "each", "ed", "edu", "effect", "eg", "eight", "eighty", "either",
+        "else", "elsewhere", "end", "ending", "enough", "especially", "et", "et-al", "etc", "even", "ever", "every",
+        "everybody", "everyone", "everything", "everywhere", "ex", "except",
+        "f", "far", "few", "ff", "fifth", "first", "five", "fix", "followed", "following", "follows", "for", "former",
+        "formerly", "forth", "found", "four", "from", "further", "furthermore",
+        "g", "gave", "get", "gets", "getting", "give", "given", "gives", "giving", "go", "goes", "gone", "got", "gotten",
+        "h", "had", "happens", "hardly", "has", "hasn't", "have", "haven't", "having", "he", "hed", "hence", "her",
+        "here", "hereafter", "hereby", "herein", "heres", "hereupon", "hers", "herself", "hes", "hi", "hid", "him",
+        "himself", "his", "hither", "home", "how", "howbeit", "however", "hundred",
+        "i", "id", "ie", "if", "i'll", "im", "immediate", "immediately", "importance", "important", "in", "inc",
+        "indeed", "index", "information", "instead", "into", "invention", "inward", "is", "isn't", "it", "itd", "it'll",
+        "its", "itself", "i've",
+        "j", "just",
+        "k", "keep", "keeps", "kept", "kg", "km", "know", "known", "knows",
+        "l", "largely", "last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let", "lets", "like",
+        "liked", "likely", "line", "little", "'ll", "look", "looking", "looks", "ltd",
+        "m", "made", "mainly", "make", "makes", "many", "may", "maybe", "me", "mean", "means", "meantime", "meanwhile",
+        "merely", "mg", "might", "million", "miss", "ml", "more", "moreover", "most", "mostly", "mr", "mrs", "much",
+        "mug", "must", "my", "myself",
+        "n", "na", "name", "namely", "nay", "nd", "near", "nearly", "necessarily", "necessary", "need", "needs",
+        "neither", "never", "nevertheless", "new", "next", "nine", "ninety", "no", "nobody", "non", "none", "nonetheless",
+        "noone", "nor", "normally", "nos", "not", "noted", "nothing", "now", "nowhere",
+        "o", "obtain", "obtained", "obviously", "of", "off", "often", "oh", "ok", "okay", "old", "omitted", "on", "once",
+        "one", "ones", "only", "onto", "or", "ord", "other", "others", "otherwise", "ought", "our", "ours", "ourselves",
+        "out", "outside", "over", "overall", "owing", "own",
+        "p", "page", "pages", "part", "particular", "particularly", "past", "per", "perhaps", "placed", "please", "plus",
+        "poorly", "possible", "possibly", "potentially", "pp", "predominantly", "present", "previously", "primarily",
+        "probably", "promptly", "proud", "provides", "put",
+        "q", "que", "quickly", "quite", "qv", "r", "ran", "rather", "rd", "re", "readily", "really", "recent",
+        "recently", "ref", "refs", "regarding", "regardless", "regards", "related", "relatively", "research",
+        "respectively", "resulted", "resulting", "results", "right", "run",
+        "s", "said", "same", "saw", "say", "saying", "says", "sec", "section", "see", "seeing", "seem", "seemed",
+        "seeming", "seems", "seen", "self", "selves", "sent", "seven", "several", "shall", "she", "shed", "she'll",
+        "shes", "should", "shouldn't", "show", "showed", "shown", "showns", "shows", "significant", "significantly",
+        "similar", "similarly", "since", "six", "slightly", "so", "some", "somebody", "somehow", "someone", "somethan",
+        "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specifically", "specified",
+        "specify", "specifying", "still", "stop", "strongly", "sub", "substantially", "successfully", "such",
+        "sufficiently", "suggest", "sup", "sure",
+        "t", "take", "taken", "taking", "tell", "tends", "th", "than", "thank", "thanks", "thanx", "that", "that'll",
+        "thats", "that've", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "thereafter",
+        "thereby", "thered", "therefore", "therein", "there'll", "thereof", "therere", "theres", "thereto", "thereupon",
+        "there've", "these", "they", "theyd", "they'll", "theyre", "they've", "think", "this", "those", "thou", "though",
+        "thoughh", "thousand", "throug", "through", "throughout", "thru", "thus", "til", "tip", "to", "together", "too",
+        "took", "toward", "towards", "tried", "tries", "truly", "try", "trying", "ts", "twice", "two",
+        "u", "un", "under", "unfortunately", "unless", "unlike", "unlikely", "until", "unto", "up", "upon", "ups", "us",
+        "use", "used", "useful", "usefully", "usefulness", "uses", "using", "usually",
+        "v", "value", "various", "'ve", "very", "via", "viz", "vol", "vols", "vs",
+        "w", "want", "wants", "was", "wasnt", "way", "we", "wed", "welcome", "we'll", "went", "were", "werent", "we've",
+        "what", "whatever", "what'll", "whats", "when", "whence", "whenever", "where", "whereafter", "whereas",
+        "whereby", "wherein", "wheres", "whereupon", "wherever", "whether", "which", "while", "whim", "whither", "who",
+        "whod", "whoever", "whole", "who'll", "whom", "whomever", "whos", "whose", "why", "widely", "willing", "wish",
+        "with", "within", "without", "wont", "words", "world", "would", "wouldnt", "www",
+        "x",
+        "y", "yes", "yet", "you", "youd", "you'll", "your", "youre", "yours", "yourself", "yourselves", "you've",
+        "z", "zero"
+    )
+
+    private const val defaultStopListText = "defaultStopList"
+    private const val emptyStopListText = "noStopList"
+
     // =============
     // == Methods ==
     // =============
@@ -98,11 +176,11 @@ object Options {
                 OPTION_STOP_LIST -> {
                     if(optionValue == "empty") {
                         stopList = emptyList()
-                        svdFilename = svdFilename.replace("defaultStopList", "noStopList")
+                        svdFilename = svdFilename.replace(defaultStopListText, emptyStopListText)
                     }
                     else {
                         stopList = File(optionValue).readLines()
-                        svdFilename = svdFilename.replace("defaultStopList", File(optionValue).nameWithoutExtension)
+                        svdFilename = svdFilename.replace(defaultStopListText, File(optionValue).nameWithoutExtension)
                     }
 
                 }
@@ -117,8 +195,8 @@ object Options {
     private fun setDefaultOptions() {
         termWeightingStrategy = LogEntropyWeighting()
         inputRootDirectory = File("inputBig/grbl")
-        stopList = File("stoplists/defaultStopList.txt").readLines()
-        svdFilename = "svd_${termWeightingStrategy.javaClass.simpleName}_defaultStopList"
+        stopList = defaultStopList
+        svdFilename = "svd_${termWeightingStrategy.javaClass.simpleName}_$defaultStopListText"
     }
 
     private fun weightingStrategy(strategyName: String): TermWeightingStrategy {
@@ -137,15 +215,15 @@ object Options {
     private fun printHelpMessage() {
         printFormattedOption(OPTION_HELP_MESSAGE, "Show possible options")
         printFormattedOption(OPTION_WEIGHTING_STRATEGY, "Specifies how to weight the entries of " +
-                "the term-document matrix. Values are 'binary', 'tf', 'tf-idf', 'log-entropy' (default)")
+                "the term-document matrix. Values are 'binary', 'tf', 'tf-idf', 'log-entropy' (default).")
         printFormattedOption(OPTION_SVD_FILENAME, "The SVD will be stored as a *.ser file " +
                 "with the provided file name. By default, the file name will mirror the options in order to make it " +
                 "identifiable. If the file already exists, it will be used to load the SVD from the *.ser file.")
         printFormattedOption(OPTION_ROOT_DIRECTORY, "The root directory where the C project is located at. " +
-                "Can be an absolute path or a relative one. (Fragile option, currently defaults to 'inputBig/grbl')")
+                "Can be an absolute path or a relative one. (Fragile option, currently defaults to 'inputBig/grbl').")
         printFormattedOption(OPTION_STOP_LIST, "The stop-list to apply. The file should contain one " +
-                "stop word per line. Passing 'empty' as an argument will not remove any words. By default, " +
-                "stoplists/defaultStopList.txt is used.")
+                "stop word per line. Pass 'empty' as an argument to not use a stop-list. By default, " +
+                "a stop-list equal to the content of 'stoplists/defaultStopList.txt' is used.")
     }
 
     private fun printFormattedOption(option: String, description: String) {
