e32085579b1be4889866567df0f3874384564ff7
kebiro <bekiroe@hotmail.de>
2020-05-15 13:31:25 +0200

    100 iter for queries (SVD gets saved on first iter, thereafter should be faster)

--- eval_queries.bat
+++ eval_queries.bat
@@ -1,5 +1,5 @@
 @echo off
 for %%r in (grblQuery.txt, grblQuery.txt) do (
-	for /L %%i in (1, 1, 2) do gradlew run -Pmyargs="@eval_params/%%r" --console=plain >> eval_results/console_%%r
+	for /L %%i in (1, 1, 100) do gradlew run -Pmyargs="@eval_params/%%r" --console=plain >> eval_results/console_%%r
 )
 pause
