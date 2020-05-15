350323a32e39965792a808eba89b26f0cf3baf5e
kebiro <bekiroe@hotmail.de>
2020-05-15 13:49:52 +0200

    Add jvmArgs to gradle.properties
    Add rest of query param files to batch file

--- eval_queries.bat
+++ eval_queries.bat
@@ -1,5 +1,5 @@
 @echo off
-for %%r in (grblQuery.txt, grblQuery.txt) do (
+for %%r in (grblQuery.txt, mosaicQuery.txt, svtQuery.txt, obsQuery.txt) do (
 	for /L %%i in (1, 1, 100) do gradlew run -Pmyargs="@eval_params/%%r" --console=plain >> eval_results/console_%%r
 )
 pause
